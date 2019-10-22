4d6da7c8e8b589c92f213f7c5043200c241f7a78
liuyousama <634308664@qq.com>
2019-10-23 07:23:28 +0800

    【refactor】：jsonGenerator

--- log/json_generator.go
+++ log/json_generator.go
@@ -1,4 +1,4 @@
-package text_generator
+package log
 
 import (
 	"encoding/json"
@@ -14,17 +14,16 @@ type JsonInfo struct {
 	CallerLine int       `json:"line"`
 	Level      string    `json:"level"`
 	Time       time.Time `json:"time"`
-	Tag        string    `json:"tag"`
 	Content    string    `json:"content"`
 }
 
-func init()  {
-	TextGenerators["json"] = new(JsonGenerator)
+func NewJsonGenerator() *JsonGenerator {
+	return new(JsonGenerator)
 }
 
 func (*JsonGenerator) Generate(tag, content, level string) string {
 	fun, file, line := GetCaller()
-	j := JsonInfo{fun,file,line,level,time.Now(),tag,content}
+	j := JsonInfo{fun,file,line,level,time.Now(), content}
 
 	b, err := json.Marshal(j)
 	if err != nil {
